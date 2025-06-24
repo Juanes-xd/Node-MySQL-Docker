4c872f28fcc86034f4e0a4b8f82c4c5eddc33028
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-06-23 23:54:43 -0500

    Init project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import { createPool } from "mysql2/promise";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const pool = createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+ 
+app.get("/ping", async (req, res) => {
+ const result = await pool.query('SELECT NOW()');
+    res.json(result[0]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
