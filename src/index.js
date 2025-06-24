008ffcd98ab37e39cff7281b974e930060c8d45f
Juanes-xd <juanestebanortizbejarano@gmail.com>
2025-06-24 00:27:19 -0500

    adding security

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import { createPool } from "mysql2/promise";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.APP_DOCKER_PORT;
 
 const pool = createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: process.env.DB_DOCKER_PORT,
 });
 
 app.get("/", (req, res) => {
